a4aadee583540f84f79a9228cee9e2216874467f
akhancode <akhan.code@gmail.com>
2024-11-13 13:20:01 +0530

    userprofile

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express();
 
 const authRoute = require("./auth.route");
 const taskRoute = require("./task.route");
+const userRoute = require("./user.route");
 const columnRoute = require("./column.route");
 
 const authMiddleware = require("../middleware/authMiddlware");
@@ -11,6 +12,7 @@ const authMiddleware = require("../middleware/authMiddlware");
 
 router.use("/api/task",authMiddleware,taskRoute);
 router.use("/api/column",authMiddleware,columnRoute);
+router.use("/api/user",authMiddleware,userRoute);
 
 router.use("/api/auth",authRoute);
 
